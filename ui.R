199fc5c13bee318ae37bf464b3ae14dfc8a1439e
dtharvey <harvey@depauw.edu>
2025-08-07 13:38:55 -0400

    revisions based on style guide
    adjustments to maintain consistency between learning modules given the project's style guide.

--- ui.R
+++ ui.R
@@ -10,126 +10,132 @@ ui = navbarPage("AC 3.0: Distributions of Data",
                 
                 # open introduction tab
                 tabPanel("Introduction",
-                 fluidRow(
-                  withMathJax(),
-                  column(width = 6,
-                   wellPanel(
-                     includeHTML("text/introduction.html")
-                )
-                ),
-                  column(width = 6,
-                    align = "center",
-                    plotOutput("introplot", height = "700px")
-                )
-                )
-                ), # close introduction tab
+                         fluidRow(
+                           withMathJax(),
+                           column(width = 6,
+                                  wellPanel(
+                                    div(class = "scrollable-well",
+                                        class = "html-fragment",
+                                        includeHTML("text/introduction.html")
+                                    ))),
+                           column(width = 6,
+                                  align = "center",
+                                  plotOutput("introplot", height = "600px")
+                           ))), # close introduction tab
                 
                 # open first activity tab
                 tabPanel("Normal Distribution",
-                  column(width = 6,
-                    wellPanel(
-                      includeHTML("text/activity1.html")
-                )     
-                ),
-                
-                  column(width = 6,
-                         align = "center",
-                    splitLayout(
-                      sliderInput("cus_mean",
-                                "average value for x",
-                                min = 1.7, max = 2.3, 
-                                value = 2, step = 0.01,
-                                width = "200px"),
-                      sliderInput("cus_sd", "standard deviation for x",
-                                  min = 0.01, max = 0.1, 
-                                  value = 0.055, step = 0.001,
-                                  width = "200px"),
-                      downloadButton("cusdata","download data")
-                ),
-                      plotOutput("activity1plot", height = "600px")
-                )
-                ), # close first activity tab
+                         fluidRow(
+                         column(width = 6,
+                                wellPanel(
+                                  div(class = "scrollable-well",
+                                      class = "html-fragment",
+                                      includeHTML("text/activity1.html")
+                                  ))),
+                         
+                         column(width = 6,
+                                align = "center",
+                                splitLayout(
+                                  sliderInput("cus_mean",
+                                              "average value for x",
+                                              min = 1.7, max = 2.3, 
+                                              value = 2, step = 0.01,
+                                              width = "200px"),
+                                  sliderInput("cus_sd", 
+                                              "standard deviation for x",
+                                              min = 0.01, max = 0.1, 
+                                              value = 0.055, step = 0.001,
+                                              width = "200px"),
+                                  downloadButton("cusdata","download data")
+                                ),
+                                plotOutput("activity1plot", height = "500px")
+                         ))), # close first activity tab
                 
                 # open second activity tab
                 tabPanel("Poisson Distribution",
-                  column(width = 6,
-                    wellPanel(
-                      includeHTML("text/activity2.html")
-                )     
-                ),
-                         
-                 column(width = 6,
-                  align = "center",
-                  splitLayout(
-                  sliderInput("lambda",
-                              "average rate: particles per 7 s", 
-                              min = 0, max = 13, value = 13, 
-                              step = 0.1),
-                  downloadButton("betadata","download data")
+                         fluidRow(
+                         column(width = 6,
+                                wellPanel(
+                                  div(class = "scrollable-well",
+                                      class = "html-fragment",
+                                      includeHTML("text/activity2.html")
+                                  ))),
+                         column(width = 6,
+                                align = "center",
+                                splitLayout(
+                                  sliderInput("lambda",
+                                              "average rate: particles per 7 s", 
+                                              min = 0, max = 13, 
+                                              value = 13, 
+                                              step = 0.1),
+                                  downloadButton("betadata",
+                                                 "download data")
                                 ),
-                  
                                 plotOutput("activity2plot", 
-                                           height = "600px")
-                )
-                ), # close second activity tab
+                                           height = "500px")
+                         ))), # close second activity tab
                 
                 # open third activity tab
                 tabPanel("Uniform Distribution",
-                  column(width = 6,
-                    wellPanel(
-                      includeHTML("text/activity3.html")
-                )     
-                ),
+                         fluidRow(
+                         column(width = 6,
+                                wellPanel(
+                                  div(class = "scrollable-well",
+                                      class = "html-fragment",
+                                      includeHTML("text/activity3.html")
+                                  ))),
                          
-                   column(width = 6,
-                    align = "center",
-                      splitLayout(
-                       sliderInput("bins",
-                                   label = "number of bins",
-                                   min = 1, max = 20, 
-                                   value = 10, step= 1, 
-                                   width = "400px"),
-                       downloadButton("pipetdata","download data")
-                              ),
+                         column(width = 6,
+                                align = "center",
+                                splitLayout(
+                                  sliderInput("bins",
+                                              label = "number of bins",
+                                              min = 1, max = 20, 
+                                              value = 10, step= 1, 
+                                              width = "400px"),
+                                  downloadButton("pipetdata","download data")
+                                ),
                                 plotOutput("activity3plot", 
-                                           height = "600px")
-                         )
-                ), # close third activity tab
+                                           height = "500px"))
+                )), # close third activity tab
                 
                 # open fourth activity tab
                 tabPanel("Binomial Distribution",
-                  column(width = 6,
-                    wellPanel(
-                               includeHTML("text/activity4.html")
-                                )     
-                         ),
+                         fluidRow(
+                         column(width = 6,
+                                wellPanel(
+                                  div(class = "scrollable-well",
+                                      class = "html-fragment",
+                                      includeHTML("text/activity4.html")
+                                  ))),
                          
-                  column(width = 6,
-                         align = "center",
-                    splitLayout(
-                                sliderInput("size",
-                                            "number of carbon atoms (n)",
-                                            min = 1, max = 50, value = 25,
-                                            step = 1, width = "200px"),
-                                sliderInput("prob",
-                                            "probability of C-13 (p)",
-                                            min = 0, max = 1, value = 0.5,
-                                            step = 0.001, width = "200px"),
-                                downloadButton("choldata","download data")
+                         column(width = 6,
+                                align = "center",
+                                splitLayout(
+                                  sliderInput("size",
+                                              "number of carbon atoms (n)",
+                                              min = 1, max = 50, value = 25,
+                                              step = 1, width = "220px"),
+                                  sliderInput("prob",
+                                              "probability of C-13 (p)",
+                                              min = 0, max = 1, value = 0.5,
+                                              step = 0.001, width = "200px"),
+                                  downloadButton("choldata","download data")
                                 ),
                                 plotOutput("activity4plot", 
-                                           height = "600px")
+                                           height = "500px")
                          )
-                ), # close fourth activity tab
+                )), # close fourth activity tab
                 
                 # open wrap-up tab
                 tabPanel("Wrapping Up",
+                         fluidRow(
                          column(width = 6,
-                          wellPanel(id = "wrapupPanel",
-                            style = "overflow-y:scroll; max-height: 700px",
-                                  includeHTML("text/wrapup.html")
-                                )     
-                         ),
+                                wellPanel(
+                                  div(class = "scrollable-well",
+                                      class = "html-fragment",
+                                      includeHTML("text/wrapup.html")
+                                  ))),
                          
                          column(width = 6,
                                 align = "center",
@@ -137,9 +143,8 @@ ui = navbarPage("AC 3.0: Distributions of Data",
                                   # inputs here
                                 ),
                                 plotOutput("wrapupplot1", 
-                                           height = "600px")
-                         )
-                ) # close wrap-up tab
-              
-  ) # close navbarPage
+                                           height = "550px")
+                         )) # close wrap-up tab
+                
+)) # close navbarPage
 
